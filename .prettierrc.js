7b6f5b89065ca10fc401ba708bd2802f3e79b091
Tim R <timothy.roettger@adhocteam.us>
2023-07-21 16:47:08 -0500

    In progress

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSameLine: true,
+  bracketSpacing: true,
+  parser: 'typescript',
+  printWidth: 110,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+}
