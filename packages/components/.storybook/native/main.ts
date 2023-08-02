07500ee0f18704cb0669e9cf87f452e34a8e278c
Narin Ratana <narin@adhocteam.us>
2023-08-01 21:53:15 -0700

    Storybook setup for web and native with TypeScript

---
+++ packages/components/.storybook/native/main.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: [
+    "../../src/components/**/*.stories.mdx",
+    "../../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-ondevice-controls",
+    "@storybook/addon-ondevice-actions",
+  ],
+};
---
+++ packages/components/src/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { registerRootComponent } from 'expo';
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const storybookEnabled = process.env.STORYBOOK_ENABLED === "true";
+
+const Index = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+};
+
+let EntryPoint = Index;
+
+if (storybookEnabled) {
+  const StorybookUI = require("../.storybook/native").default;
+  EntryPoint = () => {
+    return (
+      <View style={{ flex: 1 }}>
+        <StorybookUI />
+      </View>
+    );
+  };
+}
+
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(EntryPoint);
