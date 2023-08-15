7e1ee1a6f7919e36363b1d20262bf948bdd69b00
Narin Ratana <narin@adhocteam.us>
2023-08-15 15:24:10 -0700

    Fix storybook entrypoint. Add MyButton export to index.tsx as example

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -1,32 +1,15 @@
 import React from 'react'
-import { registerRootComponent } from 'expo'
 import { Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-const storybookEnabled = process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true'
-
 const App = () => {
+  const StorybookUI = require('../.storybook/native').default
   return (
-    <SafeAreaView>
-      <Text>Hello world</Text>
-    </SafeAreaView>
+    <View style={{ flex: 1 }}>
+      <StorybookUI />
+    </View>
   )
 }
 
-let EntryPoint = App
+export default App
 
-if (storybookEnabled) {
-  const StorybookUI = require('../.storybook/native').default
-  EntryPoint = () => {
-    return (
-      <View style={{ flex: 1 }}>
-        <StorybookUI />
-      </View>
-    )
-  }
-}
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(EntryPoint)
--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -1,1 +1,16 @@
 // Import components here so they can be exported through npm
+import { registerRootComponent } from 'expo'
+
+import App from './App'
+
+const storybookEnabled = process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true'
+
+if (storybookEnabled) {
+  // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+  // It also ensures that whether you load the app in Expo Go or in a native build,
+  // the environment is set up appropriately
+  registerRootComponent(App)
+}
+
+
+export { MyButton } from 'components/Button/Button'
