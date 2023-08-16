531817785b8e54dd7784ffc24c96c739a43d922f
Tim R <timothy.roettger@adhocteam.us>
2023-08-16 14:55:44 -0500

    Readding i18n after main merge killed it (and .yarn mess)

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { I18nextProvider } from 'react-i18next'
 import i18n from 'utils/translation/i18n'
@@ -7,11 +7,14 @@ import i18n from 'utils/translation/i18n'
 const App = () => {
   const StorybookUI = require('../.storybook/native').default
   return (
-    <View style={{ flex: 1 }}>
-      <StorybookUI />
-    </View>
+    <I18nextProvider i18n={i18n}>
+      <SafeAreaView>
+        <View style={{ flex: 1 }}>
+          <StorybookUI />
+        </View>
+      </SafeAreaView>
+    </I18nextProvider>
   )
 }
 
 export default App
-
