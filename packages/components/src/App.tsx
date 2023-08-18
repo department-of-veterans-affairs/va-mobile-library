bfb54498ebe067c90b36c98cf0ad74abfd237063
Tim R <timothy.roettger@adhocteam.us>
2023-08-18 16:41:45 -0500

    Adding splash screen to make github happy (and stop RN warnings)

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { View } from 'react-native'
 import { I18nextProvider } from 'react-i18next'
 import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen';
 
 import i18n from 'utils/translation/i18n'
+
+SplashScreen.preventAutoHideAsync()
 
 const App = () => {
   // Loads in custom fonts async
@@ -12,11 +15,22 @@ const App = () => {
     'SourceSansPro-Regular': require('./assets/fonts/SourceSansPro/SourceSansPro-Regular.ttf'),
   })
 
+  // Holds rendering until custom fonts load
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   const StorybookUI = require('../.storybook/native').default
 
   return (
     <I18nextProvider i18n={i18n}>
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
         <StorybookUI />
       </View>
     </I18nextProvider>
