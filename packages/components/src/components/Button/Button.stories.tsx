d63eb961e0752e6606abcb9abd194a58a23230c7
Narin Ratana <narin@adhocteam.us>
2023-08-23 08:37:58 -0700

    Test linting error

--- packages/components/src/components/Button/Button.stories.tsx
+++ packages/components/src/components/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react-native'
 import { MyButton, MyButtonProps } from './Button'
-import { View } from 'react-native'
 import { generateDocs } from '../../utils'
+import { View } from 'react-native'
 
 const meta: Meta<MyButtonProps> = {
   title: 'Button',
