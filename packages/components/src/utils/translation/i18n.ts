f7a3dd3dd279374723e1851aafd69f9b2cb6b363
Tim R <timothy.roettger@adhocteam.us>
2023-08-25 11:59:32 -0500

    0.0.4 test

--- packages/components/src/utils/translation/i18n.ts
+++ packages/components/src/utils/translation/i18n.ts
@@ -1,11 +1,11 @@
-import * as RNLocalize from 'expo-localization'
+import { getLocales } from 'expo-localization'
 import { initReactI18next } from 'react-i18next'
 import i18n from 'i18next'
 
 import * as enTranslation from './en.json'
 
 const fallbackLanguage = { languageTag: 'en', isRTL: false }
-const defaultLanguage = RNLocalize.getLocales()[0] || fallbackLanguage
+const defaultLanguage = getLocales()[0] || fallbackLanguage
 
 export const resources = {
   en: { translation: enTranslation },
