93749bddd62cfde3095bd64afe3e3034f9742304
Tim R <timothy.roettger@adhocteam.us>
2023-08-28 18:19:10 -0500

    0.0.8 with new localization test

--- packages/components/src/utils/translation/i18n.ts
+++ packages/components/src/utils/translation/i18n.ts
@@ -1,37 +1,30 @@
 import { initReactI18next } from 'react-i18next'
 import i18n from 'i18next'
+import RNLanguageDetector from '@os-team/i18next-react-native-language-detector';
 
 import * as enTranslation from './en.json'
-
-let localization
-try {
-  localization = require('expo-localization')
-} catch {
-  localization = require('react-native-localize')
-}
-
-const fallbackLanguage = { languageTag: 'en', isRTL: false }
-const defaultLanguage = localization.getLocales()[0] || fallbackLanguage
 
 export const resources = {
   en: { translation: enTranslation },
 }
 
 // Initialize the internationalization library
-i18n.use(initReactI18next).init({
-  lng: defaultLanguage.languageTag,
-  resources,
-  keySeparator: false,
-  fallbackLng: 'en',
-  compatibilityJSON: 'v3',
-  debug: true,
-  interpolation: {
-    escapeValue: false,
-    formatSeparator: ',',
-  },
-  react: {
-    useSuspense: true,
-  },
-})
+i18n
+  .use(RNLanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    keySeparator: false,
+    fallbackLng: 'en',
+    compatibilityJSON: 'v3',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ',',
+    },
+    react: {
+      useSuspense: true,
+    },
+  })
 
 export default i18n
