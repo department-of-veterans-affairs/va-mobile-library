2843b060525a298b155936f20dd545efa4bd02f8
Tim R <timothy.roettger@adhocteam.us>
2023-09-07 12:36:15 -0500

    Tentative progress, not working--errors on render fn

--- packages/components/src/components/SegmentedControl/SegmentedControl.test.tsx
+++ packages/components/src/components/SegmentedControl/SegmentedControl.test.tsx
@@ -1,6 +1,41 @@
-import { sum } from '../../sum'
+import 'react-native'
+import { RenderAPI, render, waitFor } from '@testing-library/react-native'
+import React from 'react'
+// Note: test renderer must be required after react-native.
+import 'jest-styled-components'
+import { ReactTestInstance } from 'react-test-renderer'
+import Mock = jest.Mock
 
-// Placeholder till component exists so the test will pass
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+import { SegmentedControl, SegmentedControlProps } from './SegmentedControl'
+
+describe('SegmentedControl', () => {
+  let component: RenderAPI
+  let testInstance: ReactTestInstance
+  let onChangeSpy: Mock
+
+  beforeEach(() => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onChangeSpy = jest.fn(() => {})
+
+    component = render<SegmentedControlProps>(
+      <SegmentedControl
+        labels={['tab0', 'tab1']}
+        onChange={onChangeSpy}
+        selected={0}
+      />,
+    )
+
+    testInstance = component.UNSAFE_root
+  })
+
+  it('initializes correctly', async () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('should call onChange', async () => {
+    await waitFor(() => {
+      testInstance.findByType(SegmentedControl).props.onChange()
+      expect(onChangeSpy).toBeCalled()
+    })
+  })
 })
