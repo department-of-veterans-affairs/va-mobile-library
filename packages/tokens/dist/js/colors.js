29698de152244fe9fcd7771cd3119641d7c6d299
Narin Ratana <narin@adhocteam.us>
2023-09-22 10:29:25 -0700

    Change from prepublish to prepack. Bump version to 0.0.6.

---
+++ packages/tokens/dist/js/colors.js
@@ -0,0 +1,16 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 22 Sep 2023 17:08:33 GMT
+ */
+
+export const white = "#ffffff";
+export const gray = "#5b616b";
+export const grayDark = "#323a45";
+export const grayMedium = "#757575";
+export const grayLight = "#aeb0b5";
+export const grayLightAlt = "#eeeeee";
+export const grayLighter = "#d6d7d9";
+export const grayLightest = "#f1f1f1";
+export const grayWarmDark = "#494440";
+export const grayWarmLight = "#e4e2e0";
+export const grayCoolLight = "#dce4ef";
