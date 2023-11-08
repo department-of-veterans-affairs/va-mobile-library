73615cbe8ed308773da3056de6f6e6d64472ed7e
Narin Ratana <narin@adhocteam.us>
2023-11-07 17:32:18 -0800

    Add unit tests

---
+++ packages/components/src/components/VAButton/VAButton.test.tsx
@@ -0,0 +1,39 @@
+import 'react-native'
+import { RenderAPI, fireEvent, render } from '@testing-library/react-native'
+import React from 'react'
+// Note: test renderer must be required after react-native.
+import 'jest-styled-components'
+import { ReactTestInstance } from 'react-test-renderer'
+
+import { VAButton } from './VAButton'
+
+describe('VAButton', () => {
+  let component: RenderAPI
+  let testInstance: ReactTestInstance
+
+  const onPressSpy = jest.fn()
+
+  const initializeTestInstance = (): void => {
+    component = render(<VAButton label="Button text" onPress={onPressSpy} />)
+
+    testInstance = component.UNSAFE_root
+  }
+
+  beforeEach(() => {
+    initializeTestInstance()
+  })
+
+  it('initializes correctly', async () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('should call onChange', async () => {
+    fireEvent.press(testInstance)
+    expect(onPressSpy).toBeCalled()
+  })
+
+  it('should render label', async () => {
+    component.debug()
+    expect(component.findByText('Button text')).toBeTruthy()
+  })
+})
