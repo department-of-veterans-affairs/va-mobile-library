ce5d3a52521eee034449f39bfa1a41d302887507
Narin Ratana <narin@adhocteam.us>
2023-11-16 09:06:28 -0800

    Group variant tests

--- packages/components/src/components/Button/Button.test.tsx
+++ packages/components/src/components/Button/Button.test.tsx
@@ -29,243 +29,255 @@ describe('Button', () => {
     expect(component.findByText('Button text')).toBeTruthy()
   })
 
-  it('should render Primary variant by default', async () => {
-    component = render(<Button label={label} onPress={onPressSpy} />)
+  describe('Primary variant', () => {
+    it('should render Primary variant by default', async () => {
+      component = render(<Button label={label} onPress={onPressSpy} />)
 
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
-    const { backgroundColor, borderWidth } = button.props.style
-    const { color } = text.props.style
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
+      const { backgroundColor, borderWidth } = button.props.style
+      const { color } = text.props.style
 
-    expect(backgroundColor).toEqual(
-      DesignTokens.colorUswdsSystemColorBlueVivid60,
-    )
-    expect(borderWidth).toEqual(0)
-    expect(color).toEqual(DesignTokens.colorGrayLightest)
+      expect(backgroundColor).toEqual(
+        DesignTokens.colorUswdsSystemColorBlueVivid60,
+      )
+      expect(borderWidth).toEqual(0)
+      expect(color).toEqual(DesignTokens.colorGrayLightest)
+    })
+
+    it('should render Primary variant in pressed state', async () => {
+      component = render(
+        <Button label={label} onPress={onPressSpy} testOnlyPressed={true} />,
+      )
+
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
+      const { backgroundColor, borderWidth } = button.props.style
+      const { color } = text.props.style
+
+      expect(backgroundColor).toEqual(
+        DesignTokens.colorUswdsSystemColorBlueWarmVivid80,
+      )
+      expect(borderWidth).toEqual(0)
+      expect(color).toEqual(DesignTokens.colorGrayLightest)
+    })
   })
 
-  it('should render Primary pressed variant', async () => {
-    component = render(
-      <Button label={label} onPress={onPressSpy} testOnlyPressed={true} />,
-    )
+  describe('Base Variant', () => {
+    it('should render Base variant', async () => {
+      component = render(
+        <Button
+          label={label}
+          onPress={onPressSpy}
+          buttonType={ButtonVariants.Base}
+        />,
+      )
 
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
-    const { backgroundColor, borderWidth } = button.props.style
-    const { color } = text.props.style
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
 
-    expect(backgroundColor).toEqual(
-      DesignTokens.colorUswdsSystemColorBlueWarmVivid80,
-    )
-    expect(borderWidth).toEqual(0)
-    expect(color).toEqual(DesignTokens.colorGrayLightest)
+      const { backgroundColor, borderWidth } = button.props.style
+      const { color } = text.props.style
+      expect(backgroundColor).toEqual(DesignTokens.colorGrayMedium)
+      expect(borderWidth).toEqual(0)
+      expect(color).toEqual(DesignTokens.colorGrayLightest)
+    })
+
+    it('should render Base variant in pressed state', async () => {
+      component = render(
+        <Button
+          label={label}
+          onPress={onPressSpy}
+          buttonType={ButtonVariants.Base}
+          testOnlyPressed={true}
+        />,
+      )
+
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
+      const { backgroundColor, borderWidth } = button.props.style
+      const { color } = text.props.style
+
+      expect(backgroundColor).toEqual(DesignTokens.colorUswdsSystemColorGray80)
+      expect(borderWidth).toEqual(0)
+      expect(color).toEqual(DesignTokens.colorGrayLightest)
+    })
   })
 
-  it('should render Base variant', async () => {
-    component = render(
-      <Button
-        label={label}
-        onPress={onPressSpy}
-        buttonType={ButtonVariants.Base}
-      />,
-    )
+  describe('Secondary Variant', () => {
+    it('should render Secondary variant', async () => {
+      component = render(
+        <Button
+          label={label}
+          onPress={onPressSpy}
+          buttonType={ButtonVariants.Secondary}
+        />,
+      )
 
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
+      const { backgroundColor, borderWidth, borderColor } = button.props.style
+      const { color } = text.props.style
 
-    const { backgroundColor, borderWidth } = button.props.style
-    const { color } = text.props.style
-    expect(backgroundColor).toEqual(DesignTokens.colorGrayMedium)
-    expect(borderWidth).toEqual(0)
-    expect(color).toEqual(DesignTokens.colorGrayLightest)
+      expect(backgroundColor).toEqual(DesignTokens.colorWhite)
+      expect(borderColor).toEqual(DesignTokens.colorUswdsSystemColorBlueVivid60)
+      expect(borderWidth).toEqual(2)
+      expect(color).toEqual(DesignTokens.colorUswdsSystemColorBlueVivid60)
+    })
+
+    it('should render Secondary variant in pressed state', async () => {
+      component = render(
+        <Button
+          label={label}
+          onPress={onPressSpy}
+          buttonType={ButtonVariants.Secondary}
+          testOnlyPressed={true}
+        />,
+      )
+
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
+      const { backgroundColor, borderColor, borderWidth } = button.props.style
+      const { color } = text.props.style
+
+      expect(backgroundColor).toEqual(DesignTokens.colorWhite)
+      expect(borderColor).toEqual(
+        DesignTokens.colorUswdsSystemColorBlueWarmVivid80,
+      )
+      expect(borderWidth).toEqual(2)
+      expect(color).toEqual(DesignTokens.colorUswdsSystemColorBlueWarmVivid80)
+    })
   })
 
-  it('should render Base pressed variant', async () => {
-    component = render(
-      <Button
-        label={label}
-        onPress={onPressSpy}
-        buttonType={ButtonVariants.Base}
-        testOnlyPressed={true}
-      />,
-    )
+  describe('Destructive Variant', () => {
+    it('should render Destructive variant', async () => {
+      component = render(
+        <Button
+          label={label}
+          onPress={onPressSpy}
+          buttonType={ButtonVariants.Destructive}
+        />,
+      )
 
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
-    const { backgroundColor, borderWidth } = button.props.style
-    const { color } = text.props.style
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
 
-    expect(backgroundColor).toEqual(DesignTokens.colorUswdsSystemColorGray80)
-    expect(borderWidth).toEqual(0)
-    expect(color).toEqual(DesignTokens.colorGrayLightest)
+      const { backgroundColor, borderWidth, borderColor } = button.props.style
+      const { color } = text.props.style
+
+      expect(backgroundColor).toEqual(
+        DesignTokens.colorUswdsSystemColorRedVivid60,
+      )
+      expect(borderColor).toEqual('none')
+      expect(borderWidth).toEqual(0)
+      expect(color).toEqual(DesignTokens.colorGrayLightest)
+    })
+
+    it('should render Destructive variant in pressed state', async () => {
+      component = render(
+        <Button
+          label={label}
+          onPress={onPressSpy}
+          buttonType={ButtonVariants.Destructive}
+          testOnlyPressed={true}
+        />,
+      )
+
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
+      const { backgroundColor, borderWidth } = button.props.style
+      const { color } = text.props.style
+
+      expect(backgroundColor).toEqual(
+        DesignTokens.colorUswdsSystemColorRedVivid80,
+      )
+      expect(borderWidth).toEqual(0)
+      expect(color).toEqual(DesignTokens.colorGrayLightest)
+    })
   })
 
-  it('should render Destructive variant', async () => {
-    component = render(
-      <Button
-        label={label}
-        onPress={onPressSpy}
-        buttonType={ButtonVariants.Destructive}
-      />,
-    )
+  describe('White Variant', () => {
+    it('should render White variant', async () => {
+      component = render(
+        <Button
+          label={label}
+          onPress={onPressSpy}
+          buttonType={ButtonVariants.White}
+        />,
+      )
 
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
+      const { backgroundColor, borderWidth, borderColor } = button.props.style
+      const { color } = text.props.style
 
-    const { backgroundColor, borderWidth, borderColor } = button.props.style
-    const { color } = text.props.style
+      expect(backgroundColor).toEqual(DesignTokens.colorGrayLightest)
+      expect(borderColor).toEqual('none')
+      expect(borderWidth).toEqual(0)
+      expect(color).toEqual(DesignTokens.colorBlack)
+    })
 
-    expect(backgroundColor).toEqual(
-      DesignTokens.colorUswdsSystemColorRedVivid60,
-    )
-    expect(borderColor).toEqual('none')
-    expect(borderWidth).toEqual(0)
-    expect(color).toEqual(DesignTokens.colorGrayLightest)
+    it('should render White variant in pressed state', async () => {
+      component = render(
+        <Button
+          label={label}
+          onPress={onPressSpy}
+          buttonType={ButtonVariants.White}
+          testOnlyPressed={true}
+        />,
+      )
+
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
+      const { backgroundColor, borderWidth } = button.props.style
+      const { color } = text.props.style
+
+      expect(backgroundColor).toEqual(DesignTokens.colorUswdsSystemColorGray30)
+      expect(borderWidth).toEqual(0)
+      expect(color).toEqual(DesignTokens.colorBlack)
+    })
   })
 
-  it('should render Destructive pressed variant', async () => {
-    component = render(
-      <Button
-        label={label}
-        onPress={onPressSpy}
-        buttonType={ButtonVariants.Destructive}
-        testOnlyPressed={true}
-      />,
-    )
+  describe('BaseSecondary Variant', () => {
+    it('should render BaseSecondary variant', async () => {
+      component = render(
+        <Button
+          label={label}
+          onPress={onPressSpy}
+          buttonType={ButtonVariants.BaseSecondary}
+        />,
+      )
 
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
-    const { backgroundColor, borderWidth } = button.props.style
-    const { color } = text.props.style
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
+      const { backgroundColor, borderWidth, borderColor } = button.props.style
+      const { color } = text.props.style
 
-    expect(backgroundColor).toEqual(
-      DesignTokens.colorUswdsSystemColorRedVivid80,
-    )
-    expect(borderWidth).toEqual(0)
-    expect(color).toEqual(DesignTokens.colorGrayLightest)
-  })
+      expect(backgroundColor).toEqual(DesignTokens.colorWhite)
+      expect(borderColor).toEqual(DesignTokens.colorGrayMedium)
+      expect(borderWidth).toEqual(2)
+      expect(color).toEqual(DesignTokens.colorGrayMedium)
+    })
 
-  it('should render White variant', async () => {
-    component = render(
-      <Button
-        label={label}
-        onPress={onPressSpy}
-        buttonType={ButtonVariants.White}
-      />,
-    )
+    it('should render BaseSecondary variant in pressed state', async () => {
+      component = render(
+        <Button
+          label={label}
+          onPress={onPressSpy}
+          buttonType={ButtonVariants.BaseSecondary}
+          testOnlyPressed={true}
+        />,
+      )
 
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
-    const { backgroundColor, borderWidth, borderColor } = button.props.style
-    const { color } = text.props.style
+      const button = component.getByRole('button')
+      const text = component.getByText(label)
+      const { backgroundColor, borderColor, borderWidth } = button.props.style
+      const { color } = text.props.style
 
-    expect(backgroundColor).toEqual(DesignTokens.colorGrayLightest)
-    expect(borderColor).toEqual('none')
-    expect(borderWidth).toEqual(0)
-    expect(color).toEqual(DesignTokens.colorBlack)
-  })
-
-  it('should render White pressed variant', async () => {
-    component = render(
-      <Button
-        label={label}
-        onPress={onPressSpy}
-        buttonType={ButtonVariants.White}
-        testOnlyPressed={true}
-      />,
-    )
-
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
-    const { backgroundColor, borderWidth } = button.props.style
-    const { color } = text.props.style
-
-    expect(backgroundColor).toEqual(DesignTokens.colorUswdsSystemColorGray30)
-    expect(borderWidth).toEqual(0)
-    expect(color).toEqual(DesignTokens.colorBlack)
-  })
-
-  it('should render Secondary variant', async () => {
-    component = render(
-      <Button
-        label={label}
-        onPress={onPressSpy}
-        buttonType={ButtonVariants.Secondary}
-      />,
-    )
-
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
-    const { backgroundColor, borderWidth, borderColor } = button.props.style
-    const { color } = text.props.style
-
-    expect(backgroundColor).toEqual(DesignTokens.colorWhite)
-    expect(borderColor).toEqual(DesignTokens.colorUswdsSystemColorBlueVivid60)
-    expect(borderWidth).toEqual(2)
-    expect(color).toEqual(DesignTokens.colorUswdsSystemColorBlueVivid60)
-  })
-
-  it('should render Secondary pressed variant', async () => {
-    component = render(
-      <Button
-        label={label}
-        onPress={onPressSpy}
-        buttonType={ButtonVariants.Secondary}
-        testOnlyPressed={true}
-      />,
-    )
-
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
-    const { backgroundColor, borderColor, borderWidth } = button.props.style
-    const { color } = text.props.style
-
-    expect(backgroundColor).toEqual(DesignTokens.colorWhite)
-    expect(borderColor).toEqual(
-      DesignTokens.colorUswdsSystemColorBlueWarmVivid80,
-    )
-    expect(borderWidth).toEqual(2)
-    expect(color).toEqual(DesignTokens.colorUswdsSystemColorBlueWarmVivid80)
-  })
-
-  it('should render BaseSecondary variant', async () => {
-    component = render(
-      <Button
-        label={label}
-        onPress={onPressSpy}
-        buttonType={ButtonVariants.BaseSecondary}
-      />,
-    )
-
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
-    const { backgroundColor, borderWidth, borderColor } = button.props.style
-    const { color } = text.props.style
-
-    expect(backgroundColor).toEqual(DesignTokens.colorWhite)
-    expect(borderColor).toEqual(DesignTokens.colorGrayMedium)
-    expect(borderWidth).toEqual(2)
-    expect(color).toEqual(DesignTokens.colorGrayMedium)
-  })
-
-  it('should render BaseSecondary pressed variant', async () => {
-    component = render(
-      <Button
-        label={label}
-        onPress={onPressSpy}
-        buttonType={ButtonVariants.BaseSecondary}
-        testOnlyPressed={true}
-      />,
-    )
-
-    const button = component.getByRole('button')
-    const text = component.getByText(label)
-    const { backgroundColor, borderColor, borderWidth } = button.props.style
-    const { color } = text.props.style
-
-    expect(backgroundColor).toEqual(DesignTokens.colorWhite)
-    expect(borderColor).toEqual(DesignTokens.colorUswdsSystemColorGray80)
-    expect(borderWidth).toEqual(2)
-    expect(color).toEqual(DesignTokens.colorUswdsSystemColorGray80)
+      expect(backgroundColor).toEqual(DesignTokens.colorWhite)
+      expect(borderColor).toEqual(DesignTokens.colorUswdsSystemColorGray80)
+      expect(borderWidth).toEqual(2)
+      expect(color).toEqual(DesignTokens.colorUswdsSystemColorGray80)
+    })
   })
 })
