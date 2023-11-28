4abc794de4c603c169f9648f006ab7e04b6f62f3
Narin Ratana <narin@adhocteam.us>
2023-11-27 17:02:10 -0800

    Add changelog script. Add Generate changelog step

---
+++ .github/scripts/generate-changelog.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if 'packages', 'current_package' and 'token' are provided as command line arguments
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <current_package> <token>"
+    exit 1
+fi
+
+# Assign command line arguments to variables
+packages=("components" "tokens")
+current_package="$1"
+token="$2"
+
+# Filter out current package
+filtered_array=()
+for value in "${packages[@]}"
+do
+    [[ $value != $current_package ]] && filtered_array+=($value)
+done
+
+# Add regex to each item in array
+filtered_array=( "${filtered_array[@]/%/-.*}" )
+
+# Join strings with pipe char
+joined_string=$(IFS="|"; echo "${filtered_array[*]}")
+
+# Build full regex string
+regex="(.*-(alpha|beta).*|$joined_string)"
+echo "Regex: $regex"
+
+github_changelog_generator -u department-of-veterans-affairs -p va-mobile-library -t $token --exclude-tags-regex $regex --no-unreleased
