135eb70571c37b97355b412f7b5ec9d464ca54d3
Tim R <timothy.roettger@adhocteam.us>
2023-12-05 16:14:02 -0600

    Ready for PR

--- packages/linting/index.js
+++ packages/linting/index.js
@@ -1,14 +1,8 @@
 module.exports = {
   plugins: ['deprecate'],
   rules: {
-    semi: [1, 'always'],
     'deprecate/import': [
       'warn',
-      {
-        name: 'SegmentedControl',
-        module: 'components',
-        use: 'SegmentedControl from @department-of-veterans-affairs/mobile-component-library',
-      },
       {
         name: 'VAButton',
         module: 'components',
