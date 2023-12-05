1d1c40b992230143f8b073e7856f38c13eb665b9
Tim R <timothy.roettger@adhocteam.us>
2023-12-05 12:21:57 -0600

    Ready for alpha build

--- packages/linting/index.js
+++ packages/linting/index.js
@@ -1,9 +1,19 @@
 module.exports = {
-  globals: {
-    MyGlobal: true,
-  },
-
+  plugins: ['deprecate'],
   rules: {
-    semi: [2, 'always'],
+    semi: [1, 'always'],
+    'deprecate/import': [
+      'warn',
+      {
+        name: 'SegmentedControl',
+        module: 'components',
+        use: 'SegmentedControl from @department-of-veterans-affairs/mobile-component-library',
+      },
+      {
+        name: 'VAButton',
+        module: 'components',
+        use: 'Button from @department-of-veterans-affairs/mobile-component-library',
+      },
+    ],
   },
 }
