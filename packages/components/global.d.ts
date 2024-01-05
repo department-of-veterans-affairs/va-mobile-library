0ad13ee08753c51ba6e95505f94a22d9c085f52d
Narin Ratana <narin@adhocteam.us>
2024-01-04 20:57:19 -0800

    Fix linting error

--- packages/components/global.d.ts
+++ packages/components/global.d.ts
@@ -1,6 +1,6 @@
 declare module '*.svg' {
-  import React from 'react'
   import { SvgProps } from 'react-native-svg'
+  import React from 'react'
   const content: React.FC<SvgProps>
   export default content
 }
