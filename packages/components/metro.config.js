f1f421947f8c564b4035694800e3885efa6db88e
Narin Ratana <narin@adhocteam.us>
2024-01-04 20:53:48 -0800

    Add comments to metro config

--- packages/components/metro.config.js
+++ packages/components/metro.config.js
@@ -13,17 +13,22 @@ module.exports = (async () => {
   } = await getDefaultConfig()
 
   return {
+    // Watch all files within the monorepo
     watchFolders: [workspaceRoot],
+    // SVG support
     transformer: {
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
+      // SVG support
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
+      // Let Metro know where to resolve packages and in what order
       nodeModulesPaths: [
         path.resolve(projectRoot, 'node_modules'),
         path.resolve(workspaceRoot, 'node_modules'),
       ],
+      // Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
       disableHierarchicalLookup: true,
       resolverMainFields: ['sbmodern', 'react-native', ...resolverMainFields],
     },
