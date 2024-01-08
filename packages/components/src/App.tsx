7467b9da4e7c7ebf4f213f3fd7f7bd4e4d848588
Tim R <timothy.roettger@adhocteam.us>
2024-01-08 14:48:02 -0600

    Initial test w/ expo/storybook working in Web/iOS/Android

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -64,4 +64,9 @@ const App = () => {
   )
 }
 
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App)
+
 export default App
