bdd57a3d77f5db5915b5a2692161e4c18a8e0b52
Narin Ratana <narin@adhocteam.us>
2024-01-09 12:25:16 -0800

    Add comment for *.svg global declaration

--- packages/components/global.d.ts
+++ packages/components/global.d.ts
@@ -1,3 +1,7 @@
+/** Prevents TypeScript from complaining about importing .svg files directly and
+ * tells TypeScript which props .svg's can accept
+ * See: https://github.com/kristerkari/react-native-svg-transformer?tab=readme-ov-file#using-typescript
+ */
 declare module '*.svg' {
   import { SvgProps } from 'react-native-svg'
   import React from 'react'
