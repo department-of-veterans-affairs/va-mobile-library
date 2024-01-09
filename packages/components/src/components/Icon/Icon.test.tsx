4c95c6d9c35b5797324a8d076d3b44d0d29bf2fa
Narin Ratana <narin@adhocteam.us>
2024-01-09 14:31:38 -0800

    Add props tests

--- packages/components/src/components/Icon/Icon.test.tsx
+++ packages/components/src/components/Icon/Icon.test.tsx
@@ -2,16 +2,61 @@ import { Icon } from './Icon'
 import { RenderAPI, render } from '@testing-library/react-native'
 import React from 'react'
 
+import * as DesignTokens from '@department-of-veterans-affairs/mobile-tokens'
+
 describe('Icon', () => {
   let component: RenderAPI
 
-  beforeEach(() => {
+  it('renders correctly', async () => {
     component = render(
-      <Icon name="HomeSelected" testID="myId" height={100} width={100} />,
+      <Icon
+        name="HomeSelected"
+        testID="myId"
+        height={100}
+        width={100}
+        fill={DesignTokens.colorGray}
+        fill2={DesignTokens.colorSecondaryLightest}
+        stroke={DesignTokens.colorPrimaryAltLightest}
+        maxWidth={100}
+        preventScaling={true}
+      />,
     )
+
+    expect(component).toBeTruthy()
+    const icon = component.getByTestId('myId')
+    const { name, width, height, preventScaling, fill, color, stroke } =
+      icon.props
+
+    expect(name).toBe('HomeSelected')
+    expect(width).toBe(100)
+    expect(height).toBe(100)
+    expect(preventScaling).toBe(true)
+    expect(fill).toBe(DesignTokens.colorGray)
+    expect(color).toBe(DesignTokens.colorSecondaryLightest)
+    expect(stroke).toBe(DesignTokens.colorPrimaryAltLightest)
   })
 
-  it('initializes correctly', async () => {
+  it('maxWidth is set properly when scaled', async () => {
+    component = render(
+      <Icon
+        name="HomeSelected"
+        testID="myId"
+        height={50}
+        width={50}
+        fill={DesignTokens.colorGray}
+        fill2={DesignTokens.colorSecondaryLightest}
+        stroke={DesignTokens.colorPrimaryAltLightest}
+        maxWidth={100}
+        preventScaling={false}
+      />,
+    )
+
     expect(component).toBeTruthy()
+    const icon = component.getByTestId('myId')
+    const { width, height, preventScaling } = icon.props
+
+    expect(width).toBe(100)
+    expect(height).toBe(100)
+    expect(preventScaling).toBe(false)
   })
 })
