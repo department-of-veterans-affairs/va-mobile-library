bcbace7a5ae6c2c8ba54bce0a34f54670f40dc4e
Tim R <timothy.roettger@adhocteam.us>
2024-01-10 14:18:24 -0600

    Reverting changes that didn't pan out

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -1,10 +1,11 @@
+import App from './main'
+
+const expoApp = App.default
+
+if (expoApp && App.initiateExpo) {
+  App.initiateExpo(expoApp)
+}
+
 // Export components here so they are exported through npm
-export { SegmentedControl } from './components/SegmentedControl/SegmentedControl'
 export { Button, ButtonVariants } from './components/Button/Button'
-
-
-/**
- * NEXT STEPS:
- * 1. Check iOS/Android still work *SEEMS GOOD*
- * 2. If so, clean up App.tsx/main.tsx and publish a test build to check package
- */
+export { SegmentedControl } from './components/SegmentedControl/SegmentedControl'
