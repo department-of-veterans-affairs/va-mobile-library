c1e1872c1b5456355ffc19cecad5d9e18354b177
Tim R <timothy.roettger@adhocteam.us>
2024-02-01 12:46:24 -0600

    Building out props model for input data

--- packages/components/src/components/Link/Link.tsx
+++ packages/components/src/components/Link/Link.tsx
@@ -1,28 +1,108 @@
 import { AccessibilityProps, TouchableWithoutFeedback, TouchableWithoutFeedbackProps } from 'react-native'
 import { SvgProps } from 'react-native-svg'
-import Box from './Box'
+// import Box from './Box'
 import React, { FC } from 'react'
 
 import { Icon, IconProps } from '../Icon/Icon'
-import { useTheme } from 'utils/hooks'
-import TextView, { ColorVariant, TextViewProps } from './TextView'
+// import { useTheme } from 'utils/hooks'
+// import TextView, { ColorVariant, TextViewProps } from './TextView'
+
+type CalendarData = {
+  title: string
+  startTime: number
+  endTime: number
+  location: string
+  latitude: number
+  longitude: number
+}
+
+type calendar = {
+  type: 'calendar'
+  eventData: CalendarData
+}
+
+type call = {
+  type: 'call' | 'call TTY'
+  phoneNumber: string
+}
+
+type chat = {
+  type: 'chat'
+  url: string
+}
+
+type custom = {
+  type: 'custom'
+  onPress: onPress
+}
+
+type AppointmentAddress = {
+  street: string
+  city: string
+  state: string // 2 letter abbreviation
+  zipCode: string
+}
+
+type LocationData = {
+  name: string
+  address?: AppointmentAddress
+  lat?: number
+  long?: number
+}
+
+type directions = {
+  type: 'directions'
+  locationData: LocationData
+}
+
+type text = {
+  type: 'text'
+  textNumber: string
+}
+
+type url = {
+  type: 'url'
+  url: string
+}
+
+type linkType = calendar
+              | call
+              | chat
+              | custom
+              | directions
+              | text
+              | url
+
+type onPress = {
+ /** Custom logic to overrides built in onPress logic */
+  custom?: () => void
+}
+
+type analytics = {
+  onPress?: () => void
+  onConfirm?: () => void
+  hasCalendarPermission?: () => void
+  onRequestCalendarPermission?: () => void
+  onCalendarPermissionSuccess?: () => void
+  onCalendarPermissionFailure?: () => void
+}
 
 /**
  *  Signifies the props that need to be passed in to {@link ClickForActionLink}
  */
-export type LinkButtonProps = AccessibilityProps & {
-  /** phone number or text for url that is displayed to the user, may be different than actual number or url used */
+export type LinkProps = {
+  /** Display text for the link */
   text: string
-
-  /** */
-  onPress: () => void
-
-  /** */
-  icon?: IconProps
-
-  /** Accessibility label for the link, mandatory for every element with a link role */
-  a11yLabel: string
-
+  /** Preset link types that include default icons and onPress behavior */
+  type: linkType
+  /** Optional onPress logic */
+  onPress?: onPress
+  /** Optional icon override */
+  icon?: IconProps | 'no icon'
+  /** Optional a11yLabel override; should be used for phone numbers */
+  a11yLabel?: string
+  /** Optional analytics event logging */
+  analytics?: analytics
   /** Optional TestID */
   testID?: string
 }
@@ -30,17 +110,18 @@ export type LinkButtonProps = AccessibilityProps & {
 /**
  * Reusable component used for opening native calling app, texting app, or opening a url in the browser
  */
-export const Link: FC<LinkButtonProps> = ({
+export const Link: FC<LinkProps> = ({
   text,
+  type,
   onPress,
   icon,
   a11yLabel,
-  testID,
-  ...props
+  analytics,
+  testID
 }) => {
-  const theme = useTheme()
+  // const theme = useTheme()
 
-  const textViewProps: TextViewProps = {
+  const textViewProps = {
     color: 'link',
     variant: 'MobileBody',
     ml: 4,
@@ -49,23 +130,22 @@ export const Link: FC<LinkButtonProps> = ({
   }
 
   const pressableProps: TouchableWithoutFeedbackProps = {
-    onPress,
+    onPress: onPress?.custom,
     accessibilityLabel: a11yLabel,
     accessibilityRole: 'link',
     accessible: true,
-    ...props,
   }
 
   // ON PRESS ASYNC, DOES THAT NEED ANY SPECIAL HANDLING!?
 
   return (
     <TouchableWithoutFeedback testID={testID} {...pressableProps}>
-      <Box flexDirection={'row'} py={theme.dimensions.buttonPadding} alignItems={'center'}>
+      {/* <Box flexDirection={'row'} py={theme.dimensions.buttonPadding} alignItems={'center'}>
         {icon ? <Icon fill='blue' {...icon} /> : null}
         <Box flexShrink={1}>
           <TextView {...textViewProps}>{text}</TextView>
         </Box>
-      </Box>
+      </Box> */}
     </TouchableWithoutFeedback>
   )
 }
