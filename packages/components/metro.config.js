989a61da0180984c604b696d11008f93015bf36d
Narin Ratana <narin@adhocteam.us>
2024-02-08 20:57:17 -0800

    Fix storybook.requires generation for react native

--- packages/components/metro.config.js
+++ packages/components/metro.config.js
@@ -2,6 +2,13 @@
 const { getDefaultConfig } = require('expo/metro-config')
 const path = require('path')
 
+const { generate } = require('@storybook/react-native/scripts/generate')
+
+// Generates storybook.requires, which is used to load all our stories and addons in our project.
+generate({
+  configPath: path.resolve(__dirname, './.storybook/native'),
+})
+
 // Find the project and workspace directories
 const projectRoot = __dirname
 // This can be replaced with `find-yarn-workspace-root`
@@ -31,44 +38,5 @@ module.exports = (() => {
     disableHierarchicalLookup: true,
   }
 
-  console.log(config.resolver.resolverMainFields)
-
   return config
 })()
-// // Find the project and workspace directories
-// const projectRoot = __dirname
-// // This can be replaced with `find-yarn-workspace-root`
-// const workspaceRoot = path.resolve(projectRoot, '../..')
-
-// module.exports = (() => {
-//   const {
-//     resolver: { sourceExts, assetExts, resolverMainFields },
-//   } = getDefaultConfig(__dirname)
-
-//   const config = {
-//     // Watch all files within the monorepo
-//     watchFolders: [workspaceRoot],
-//     // SVG support
-//     transformer: {
-//       babelTransformerPath: require.resolve('react-native-svg-transformer'),
-//       unstable_allowRequireContext: true,
-//     },
-//     resolver: {
-//       // SVG support
-//       assetExts: assetExts.filter((ext) => ext !== 'svg'),
-//       sourceExts: [...sourceExts, 'svg'],
-//       // Let Metro know where to resolve packages and in what order
-//       nodeModulesPaths: [
-//         path.resolve(projectRoot, 'node_modules'),
-//         path.resolve(workspaceRoot, 'node_modules'),
-//       ],
-//       // Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-//       disableHierarchicalLookup: true,
-//       resolverMainFields: ['react-native', ...resolverMainFields],
-//     },
-//   }
-
-//   console.log(config)
-
-//   return config
-// })()
