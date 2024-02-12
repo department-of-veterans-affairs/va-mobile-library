c755270dec6a4b58dc57006af4adc4d6c5cdb32d
Narin Ratana <narin@adhocteam.us>
2024-02-12 09:58:17 -0800

    Update Link story for Storybook 7. Add link to Docs

--- packages/components/src/components/Link/Link.stories.tsx
+++ packages/components/src/components/Link/Link.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react-native'
+import { Meta, StoryObj } from '@storybook/react'
 import { Platform, View } from 'react-native'
 import React from 'react'
 
@@ -12,13 +12,6 @@ const meta: Meta<LinkProps> = {
     onPress: {
       action: 'onPress event',
     },
-  },
-  parameters: {
-    docs: generateDocs({
-      name: 'Link',
-      docUrl:
-        'https://department-of-veterans-affairs.github.io/va-mobile-app/design/Components/Buttons%20and%20links/Link',
-    }),
   },
   decorators: [
     (Story) => (
@@ -68,7 +61,7 @@ const getLocation = (): string => {
 }
 
 export const Calendar: Story = {
-  storyName: 'Calendar',
+  name: 'Calendar',
   args: {
     text: 'Add to calendar',
     onPress: undefined, // Storybook sends a truthy function shell otherwise
@@ -108,7 +101,7 @@ export const CustomWithIcon: Story = {
 }
 
 export const Directions: Story = {
-  storyName: 'Directions',
+  name: 'Directions',
   args: {
     text: 'Get directions',
     onPress: undefined, // Storybook sends a truthy function shell otherwise
@@ -126,7 +119,7 @@ export const Directions: Story = {
       title: 'Title override',
     },
     a11yLabel: 'Get directions with Maps app',
-    a11yHint: 'Opens maps app with directions to the location'
+    a11yHint: 'Opens maps app with directions to the location',
   },
 }
 
