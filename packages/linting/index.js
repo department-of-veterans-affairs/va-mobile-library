52908e05ff688187cb3a1928a6f91d84a0f7d04b
Narin Ratana <narin@adhocteam.us>
2024-03-13 22:19:41 -0700

    Fix rules

--- packages/linting/index.js
+++ packages/linting/index.js
@@ -1,32 +1,23 @@
 module.exports = {
   plugins: ['deprecate'],
   rules: {
-    'deprecate/import-va-button': [
+    'deprecate/import': [
       'warn',
       {
         name: 'VAButton',
         module: 'components',
         use: 'Button from @department-of-veterans-affairs/mobile-component-library',
       },
-    ],
-    'deprecate/import-click-for-action-link': [
-      'warn',
       {
         name: 'ClickForActionLink',
         module: 'components',
         use: 'Link from @department-of-veterans-affairs/mobile-component-library',
       },
-    ],
-    'deprecate/import-click-to-call-phone-number': [
-      'warn',
       {
         name: 'ClickToCallPhoneNumber',
         module: 'components',
         use: 'Link from @department-of-veterans-affairs/mobile-component-library',
       },
-    ],
-    'deprecate/import-attachment-link': [
-      'warn',
       {
         name: 'AttachmentLink',
         module: 'components',
