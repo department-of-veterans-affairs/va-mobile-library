5c357f9476619a491815fdf2db24491efd5a2266
Narin Ratana <narin@adhocteam.us>
2024-04-15 12:44:21 -0700

    Remove initial collapsed state from story

--- packages/components/src/components/Alert/Alert.stories.tsx
+++ packages/components/src/components/Alert/Alert.stories.tsx
@@ -67,7 +67,6 @@ export const Collapsible: Story = {
     description: 'Description',
     children: children,
     collapsible: true,
-    initialCollapsedState: 'expanded',
     primaryButton: {
       label: 'Button Text',
       onPress: () => {
