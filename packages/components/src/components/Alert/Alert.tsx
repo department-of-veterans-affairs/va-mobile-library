d18b4966e8e83427f904d45457a2f2f06c20f995
Narin Ratana <narin@adhocteam.us>
2024-04-23 11:48:19 -0700

    Fix aria-expanded value

--- packages/components/src/components/Alert/Alert.tsx
+++ packages/components/src/components/Alert/Alert.tsx
@@ -234,7 +234,7 @@ export const Alert: FC<AlertProps> = ({
         <Pressable
           onPress={() => setExpanded(!expanded)}
           role="tab"
-          aria-expanded={{ expanded }}
+          aria-expanded={expanded}
           aria-label={a11yLabel}
           hitSlop={hitSlop}
           style={{ flexDirection: 'row' }}>
