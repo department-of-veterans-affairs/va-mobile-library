233b2706643f961d766bd254153571d26ec5507d
Tim R <TimRoe@users.noreply.github.com>
2024-04-24 11:41:40 -0500

    Update URL for Storybook to point to for full Alert doc

--- packages/components/src/components/Alert/Alert.tsx
+++ packages/components/src/components/Alert/Alert.tsx
@@ -60,7 +60,7 @@ export type AlertProps = {
     }
 )
 
-/** [View guidance for the Alert component on the VA Mobile Documentation Site](https://department-of-veterans-affairs.github.io/va-mobile-app/design/Components/Buttons%20and%20links/Alert) */
+/** [View guidance for the Alert component on the VA Mobile Documentation Site](https://department-of-veterans-affairs.github.io/va-mobile-app/design/Components/Alerts%20and%20progress/Alert) */
 export const Alert: FC<AlertProps> = ({
   variant,
   header,
