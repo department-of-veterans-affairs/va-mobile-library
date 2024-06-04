e00bfe9814c2553ee9cf0f802a14bbf73c3109c5
Tim R <timothy.roettger@adhocteam.us>
2024-06-04 15:15:17 -0500

    Committing coerced into story version for a record of how it was done, will be reverted

--- packages/components/src/components/Icon/Icon.stories.tsx
+++ packages/components/src/components/Icon/Icon.stories.tsx
@@ -1,11 +1,13 @@
 import { IconGallery, IconItem } from '@storybook/blocks'
 import { Meta, StoryObj } from '@storybook/react'
+import { ThemeProvider, ensure, themes } from '@storybook/theming'
 import { View } from 'react-native'
 import React from 'react'
 
 import { Icon, IconProps } from './Icon'
 import { IconMap } from './iconList'
 import { generateDocs } from '../../utils/storybook'
+import { useColorScheme } from '../../utils'
 import CustomSVG from '../../assets/svgs/custom.svg'
 
 const buildIconItems = () => {
@@ -50,7 +52,7 @@ const meta: Meta<IconProps> = {
     name: { options: Object.keys(IconMap) },
   },
   parameters: {
-    docs: generateDocs({ icons: iconGallery }),
+    docs: generateDocs({}),
   },
 }
 
@@ -66,6 +68,7 @@ export const _Preloaded: Story = {
     name: 'HomeOutlined',
   },
 }
+
 export const __Custom: Story = {
   name: 'Custom SVG',
   args: {
@@ -74,4 +77,20 @@ export const __Custom: Story = {
     fill: 'base',
     svg: CustomSVG,
   },
+}
+
+export const ___IconGallery: Story = {
+  name: 'Icon Gallery',
+  render: () => {
+    const colorScheme = useColorScheme()
+    const isDarkMode = colorScheme === 'dark'
+
+    const theme = ensure(isDarkMode ? themes.dark : themes.light)
+
+    return(
+      <ThemeProvider theme={theme} >
+        {iconGallery}
+      </ThemeProvider>
+    )
+  }
 }
