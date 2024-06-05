6ae51bbd74b10ee006ad95da71702a7bea883633
Narin Ratana <narin@adhocteam.us>
2024-06-05 09:45:51 -0700

    Add with children story

--- packages/components/src/components/LoadingIndicator/LoadingIndicator.stories.tsx
+++ packages/components/src/components/LoadingIndicator/LoadingIndicator.stories.tsx
@@ -2,7 +2,9 @@ import { Meta, StoryObj } from '@storybook/react'
 import { View } from 'react-native'
 import React from 'react'
 
+import { Link } from '../Link/Link'
 import { LoadingIndicator, LoadingIndicatorProps } from './LoadingIndicator'
+import { Spacer } from '../Spacer/Spacer'
 import { generateDocs } from '../../utils/storybook'
 
 const meta: Meta<LoadingIndicatorProps> = {
@@ -29,6 +31,14 @@ export default meta
 
 type Story = StoryObj<LoadingIndicatorProps>
 
+const children = (
+  <>
+    <Link type="call" text="000-000-0000" phoneNumber="000-000-0000" />
+    <Spacer />
+    <Link type="call TTY" text="TTY: 711" TTYnumber="711" />
+  </>
+)
+
 export const _WithText: Story = {
   name: 'With Text',
   args: {
@@ -37,4 +47,12 @@ export const _WithText: Story = {
 }
 export const __WithoutText: Story = {
   name: 'Without Text',
+}
+
+export const ___WithChildren: Story = {
+  name: 'With Children',
+  args: {
+    text: 'Loading your appointments. Call one of the numbers below for support.',
+    children,
+  },
 }
