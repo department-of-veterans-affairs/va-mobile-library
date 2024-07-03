11964829903bf8c873a3a29608b02e06474d3ff8
Tim R <timothy.roettger@adhocteam.us>
2024-07-03 12:48:55 -0500

    Merging component translations into common namespace

--- packages/components/src/utils/translation/i18n.ts
+++ packages/components/src/utils/translation/i18n.ts
@@ -15,21 +15,9 @@ if (process.env.STORYBOOK_WEB !== 'true') {
   i18n.use(RNLanguageDetector)
 }
 
+// If i18n is already initialized (in app), add our translations to the common namespace
 if (i18n.isInitializing || i18n.isInitialized) {
-  i18n.use(initReactI18next).createInstance({
-    resources,
-    keySeparator: false,
-    fallbackLng: 'en',
-    compatibilityJSON: 'v3',
-    debug: true,
-    interpolation: {
-      escapeValue: false,
-      formatSeparator: ',',
-    },
-    react: {
-      useSuspense: true,
-    },
-  })
+  i18n.addResourceBundle('en', 'common', resources.en.translation)
 } else {
   i18n.use(initReactI18next).init({
     resources,
