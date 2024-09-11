6e298b5d7190b3afc37939b6966deea38725b64a
Narin Ratana <narin@adhocteam.us>
2024-09-11 12:59:17 -0700

    Rename index to key. Improve on size comments

--- packages/components/src/components/Spacer/Spacer.tsx
+++ packages/components/src/components/Spacer/Spacer.tsx
@@ -3,21 +3,39 @@ import React, { FC } from 'react'
 
 import { spacing } from '@department-of-veterans-affairs/mobile-tokens'
 
+/**
+ * Represents the size of a spacer component. Corresponds to spacing tokens.
+ * - 'none': 0
+ * - '2xs': 4
+ * - 'xs': 8
+ * - 'sm': 12
+ * - 'md': 16
+ * - 'lg': 20
+ * - 'xl': 24
+ * - '2xl': 28
+ * - '3xl': 32
+ * - '4xl': 36
+ * - '5xl': 40
+ * - '6xl': 44
+ */
+type SpacerSize =
+  | 'none'
+  | '2xs'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | '2xl'
+  | '3xl'
+  | '4xl'
+  | '5xl'
+  | '6xl'
+
 export type SpacerProps = {
-  /** Size of the spacer, default sm */
-  size?:
-    | 'none'
-    | '2xs'
-    | 'xs'
-    | 'sm'
-    | 'md'
-    | 'lg'
-    | 'xl'
-    | '2xl'
-    | '3xl'
-    | '4xl'
-    | '5xl'
-    | '6xl'
+  /** Size of the spacer, default sm (12). @see {@link SpacerSize} for values **/
+  size?: SpacerSize
+
   /** True for horizontal spacing */
   horizontal?: boolean
 }
@@ -32,8 +50,8 @@ export const Spacer: FC<SpacerProps> = ({
   size = 'sm',
   horizontal = false,
 }) => {
-  const index = `vadsSpace${size[0].toUpperCase()}${size.slice(1)}`
-  const spacerSize: number = spacing[index as keyof typeof spacing]
+  const key = `vadsSpace${size[0].toUpperCase()}${size.slice(1)}`
+  const spacerSize: number = spacing[key as keyof typeof spacing]
 
   return (
     <View
