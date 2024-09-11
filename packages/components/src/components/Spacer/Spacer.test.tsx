5421e76fb7f7eb74719e2694069b73633cf98b04
Narin Ratana <narin@adhocteam.us>
2024-09-11 13:00:51 -0700

    Update test names

--- packages/components/src/components/Spacer/Spacer.test.tsx
+++ packages/components/src/components/Spacer/Spacer.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react-native'
 import React from 'react'
 
 describe('Spacer', () => {
-  it('renders correctly at default size vadsSpaceSm', () => {
+  it('renders correctly at default size  vadsSpaceSm (12)', () => {
     render(<Spacer />)
 
     const spacer = screen.root
@@ -12,7 +12,7 @@ describe('Spacer', () => {
     expect(spacer.props.style.width).toBe('auto')
   })
 
-  it('renders correctly at overridden size vadsSpaceLg', () => {
+  it('renders correctly at overridden size vadsSpaceLg (20)', () => {
     render(<Spacer size="lg" />)
 
     const spacer = screen.root
@@ -21,7 +21,7 @@ describe('Spacer', () => {
     expect(spacer.props.style.width).toBe('auto')
   })
 
-  it('renders horizontally at default size vadsSpaceSm', () => {
+  it('renders horizontally at default size  vadsSpaceSm (12)', () => {
     render(<Spacer horizontal />)
 
     const spacer = screen.root
@@ -30,7 +30,7 @@ describe('Spacer', () => {
     expect(spacer.props.style.width).toBe(12)
   })
 
-  it('renders horizontally at overridden size vadsSpaceLg', () => {
+  it('renders horizontally at overridden size vadsSpaceLg (20)', () => {
     render(<Spacer size="lg" horizontal />)
 
     const spacer = screen.root
