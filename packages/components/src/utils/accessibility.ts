b830f316e8f2f59cab703eb6d4f7c6c3520e6557
Narin Ratana <narin@adhocteam.us>
2024-09-25 12:51:33 -0700

    Add comments to text utils

--- packages/components/src/utils/accessibility.ts
+++ packages/components/src/utils/accessibility.ts
@@ -3,18 +3,14 @@ import { AccessibleText } from '../types/common'
 // Export related hooks
 export { useIsScreenReaderEnabled } from './hooks/useIsScreenReaderEnabled'
 
-export const getDisplayText = (text?: AccessibleText): string | undefined => {
-  if (text) {
-    return typeof text === 'object' ? text.text : text
-  }
-}
+/**
+ * Returns text that should be displayed on the screen
+ */
+export const getDisplayText = (text: AccessibleText): string =>
+  typeof text === 'string' ? text : text.text
 
-export const getA11yText = (text?: AccessibleText): string | undefined => {
-  if (text) {
-    if (typeof text === 'object') {
-      return text.a11y || text.text
-    }
-
-    return text
-  }
-}
+/**
+ * Returns override text to be read by screen readers
+ */
+export const getA11yText = (text: AccessibleText): string =>
+  typeof text === 'string' ? text : text.a11y || text.text
