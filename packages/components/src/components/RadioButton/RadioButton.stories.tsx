f3877765a1ac42d693c9e2de044dc54dc4cce848
Tim R <timothy.roettger@adhocteam.us>
2024-11-05 13:29:38 -0600

    Initial Radio button creation

---
+++ packages/components/src/components/RadioButton/RadioButton.stories.tsx
@@ -0,0 +1,108 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { View } from 'react-native'
+import React, { useState } from 'react'
+
+import { RadioButton, RadioButtonProps } from './RadioButton'
+import { generateDocs } from '../../utils/storybook'
+
+const meta: Meta<RadioButtonProps> = {
+  title: 'Radio button',
+  component: RadioButton,
+  decorators: [
+    (Story) => (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: 8,
+        }}>
+        {Story()}
+      </View>
+    ),
+  ],
+  parameters: {
+    docs: generateDocs({
+      name: 'Radio button',
+      docUrl:
+        'https://department-of-veterans-affairs.github.io/va-mobile-app/design/Components/Selection%20and%20Input/Checkbox/',
+    }),
+  },
+}
+
+export default meta
+
+type Story = StoryObj<RadioButtonProps>
+
+const statefulComponentRenderer = (props: RadioButtonProps) => {
+  const { error, header, hint, items, required, tile } = props
+
+  const [selectedItems, setSelectedItems] = useState<(string | number)[]>([])
+
+  return (
+    <RadioButton
+      items={items}
+      selectedItems={selectedItems}
+      error={error}
+      header={header}
+      hint={hint}
+      onSelectionChange={(selected) => setSelectedItems(selected)}
+      required={required}
+      tile={tile}
+    />
+  )
+}
+
+const items = [
+  { text: 'Option 1', description: 'Description for option 1' },
+  {
+    text: 'Option 2',
+    a11yLabel: 'Accessibility override for option 2',
+    value: '2',
+    description: {
+      text: 'Description for option 2',
+      a11yLabel: 'Accessibility override for description',
+    },
+  },
+  { text: 'Option 3' },
+  { text: 'Option 4' },
+  { text: 'Option 5' },
+  { text: 'Option 6' },
+]
+
+const simpleItems = ['Option 1', 'Option 2', 'Option 3', 'Option 4']
+
+const header = 'Header'
+const hint = { text: 'Hint text', a11yLabel: 'Accessibility override for hint' }
+const error = { text: 'Error text' }
+
+export const _Default: Story = {
+  render: statefulComponentRenderer,
+  args: {
+    header,
+    hint,
+    items,
+    required: true,
+  },
+}
+
+export const __Tile: Story = {
+  render: statefulComponentRenderer,
+  args: {
+    header,
+    hint,
+    items: simpleItems,
+    tile: true,
+  },
+}
+
+export const ___Error: Story = {
+  render: statefulComponentRenderer,
+  args: {
+    error,
+    header,
+    hint,
+    items,
+    required: true,
+  },
+}
---
+++ packages/components/src/components/shared/CheckboxRadio.tsx
@@ -0,0 +1,157 @@
+import {
+  Pressable,
+  StyleProp,
+  View,
+  ViewStyle,
+  useWindowDimensions,
+} from 'react-native'
+import { spacing } from '@department-of-veterans-affairs/mobile-tokens'
+import React, { FC } from 'react'
+
+import { CheckboxRadioProps, FormElementProps } from '../../types/forms'
+import { ComponentWrapper } from '../../wrapper'
+import {
+  Description,
+  Error,
+  Header,
+  Hint,
+  Label,
+  fontLabel,
+} from '../shared/FormText'
+import { Icon, IconProps } from '../Icon/Icon'
+import { Spacer } from '../Spacer/Spacer'
+import { useTheme } from '../../utils'
+
+export type _CheckboxRadioProps = FormElementProps &
+  CheckboxRadioProps & {
+    /** True to make checkbox appear as checked */
+    checked?: boolean
+    /** True to apply indeterminate icon to checkbox */
+    indeterminate?: boolean
+    /** True to render as a radio button */
+    radio?: boolean
+  }
+
+export const CheckboxRadio: FC<_CheckboxRadioProps> = ({
+  checked,
+  label,
+  description,
+  error,
+  header,
+  hint,
+  indeterminate,
+  onPress,
+  radio,
+  required,
+  testID,
+  tile,
+}) => {
+  const theme = useTheme()
+  const fontScale = useWindowDimensions().fontScale
+
+  /**
+   * Container styling
+   */
+  let containerStyle: ViewStyle = {
+    width: '100%',
+  }
+
+  if (error) {
+    containerStyle = {
+      ...containerStyle,
+      borderLeftWidth: spacing.vadsSpace2xs,
+      borderColor: theme.vadsColorFormsBorderError,
+      paddingLeft: spacing.vadsSpaceMd,
+    }
+  }
+
+  /**
+   * Pressable styling
+   */
+  const pressableBaseStyle: StyleProp<ViewStyle> = {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  }
+
+  const tileStyle: ViewStyle = {
+    ...pressableBaseStyle,
+    borderWidth: 2,
+    borderRadius: 4,
+    padding: spacing.vadsSpaceSm,
+    paddingRight: spacing.vadsSpaceMd,
+    borderColor: checked
+      ? theme.vadsColorFormsBorderActive
+      : theme.vadsColorFormsBorderSubtle,
+    backgroundColor: checked
+      ? theme.vadsColorFormsSurfaceActive
+      : theme.vadsColorSurfaceDefault,
+  }
+
+  /**
+   * Icon
+   */
+  const iconViewStyle: ViewStyle = {
+    // Below keeps icon aligned with first row of text, centered, and scalable
+    alignSelf: 'flex-start',
+    // TODO: Replace lineHeight with typography token
+    minHeight: fontLabel.lineHeight * fontScale,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+  let iconName: IconProps['name']
+
+  if (radio) {
+    iconName = checked ? 'RadioButtonChecked' : 'RadioButtonUnchecked'
+  } else {
+    iconName = indeterminate
+      ? 'IndeterminateCheckBox'
+      : checked
+        ? 'CheckBox'
+        : 'CheckBoxOutlineBlank'
+  }
+
+  const iconProps: IconProps = {
+    name: iconName,
+    fill:
+      checked || indeterminate
+        ? theme.vadsColorFormsForegroundActive
+        : theme.vadsColorFormsBorderDefault,
+  }
+
+  const _icon = (
+    <View style={iconViewStyle}>
+      <Icon {...iconProps} />
+    </View>
+  )
+
+  return (
+    <ComponentWrapper>
+      <View style={containerStyle} testID={testID}>
+        <Header text={header} />
+        {header && <Spacer size="xs" />}
+
+        <Hint text={hint} />
+        {hint && <Spacer size="xs" />}
+
+        <Error text={error} />
+        {error && <Spacer size="xs" />}
+
+        <Pressable
+          onPress={onPress}
+          style={tile ? tileStyle : pressableBaseStyle}
+          aria-checked={indeterminate ? 'mixed' : checked}
+          role="checkbox">
+          {_icon}
+          <Spacer size="xs" horizontal />
+          <View style={{ flexShrink: 1 }}>
+            <Label text={label} error={error} required={required} />
+            {description && <Spacer size="xs" />}
+            <Description text={description} />
+          </View>
+        </Pressable>
+      </View>
+    </ComponentWrapper>
+  )
+}
