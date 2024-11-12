5f411124a40677c7b2f7d52a98e171b3e3af1e39
Narin Ratana <narin@adhocteam.us>
2024-11-12 12:20:53 -0800

    Replace color with tone. Adjust props to allow conditional tones based on variant

--- packages/components/src/components/Text/Text.tsx
+++ packages/components/src/components/Text/Text.tsx
@@ -6,14 +6,27 @@ import {
 import { colors, font } from '@department-of-veterans-affairs/mobile-tokens'
 import React, { FC } from 'react'
 
-import { BaseColor } from '../../utils'
+import { BaseColor, useTheme } from '../../utils'
 import { SpacerSize, getSpacingToken } from '../Spacer/Spacer'
 
 type TextSizes = 'xs' | 'sm' | 'md' | 'lg'
+type BaseTones = 'default' | 'subtle' | 'inverse'
+type BodyTones = BaseTones | 'error'
 
-type BodyOrHeadingProps = {
+type BodyProps = {
   /** Variant: body, heading, display, or navigation */
-  variant?: 'body' | 'heading'
+  variant?: 'body'
+  /** Text color: default, subtle, inverse, error. Defaults to vadsColorForegroundDefault. */
+  tone?: BodyTones
+  /** Size: xs, sm, md, or lg. Defaults to 'md' for body and heading */
+  size?: TextSizes
+}
+
+type HeadingProps = {
+  /** Variant: body, heading, display, or navigation */
+  variant?: 'heading'
+  /** Text color: default, subtle, inverse. Defaults to vadsColorForegroundDefault. */
+  tone?: BaseTones
   /** Size: xs, sm, md, or lg. Defaults to 'md' for body and heading */
   size?: TextSizes
 }
@@ -21,42 +34,9 @@ type BodyOrHeadingProps = {
 type DisplayProps = {
   /** Variant: body, heading, display, or navigation */
   variant?: 'display'
+  /** Text color: default, subtle, inverse. Defaults to vadsColorForegroundDefault. */
+  tone?: BaseTones
   size?: never
-}
-
-/**
- * Convenience function to get typography token based on variant and size abbreviation
- */
-function getTypographyToken(
-  variant: BodyOrHeadingProps['variant'] | DisplayProps['variant'],
-  size: TextSizes,
-) {
-  const { typography } = font
-
-  const prefix = 'vadsFont'
-
-  const sizeMap = {
-    xs: 'Xsmall',
-    sm: 'Small',
-    md: 'Medium',
-    lg: 'Large',
-  }
-
-  let key
-
-  /** Build typography token key based on variant and size props */
-  switch (variant) {
-    case 'display':
-      key = `${prefix}Display`
-      break
-    case 'heading':
-      key = `${prefix}Heading${sizeMap[size as keyof typeof sizeMap]}`
-      break
-    default:
-      key = `${prefix}Body${sizeMap[size as keyof typeof sizeMap]}`
-  }
-
-  return typography[key as keyof typeof typography]
 }
 
 export type TextProps = {
@@ -64,29 +44,78 @@ export type TextProps = {
   /** AccessibilityLabel for the text */
   a11yLabel?: string
   /**
-   * Text color. Defaults to vadsColorForegroundDefault. See {@link colors} for possible values
-   */
-  color?: keyof typeof colors
-  /**
    * Optional bottom spacing if typography style default isn't desired.
    * @see {@link SpacerSize} for possible values
    **/
   bottomSpacing?: SpacerSize
-} & (BodyOrHeadingProps | DisplayProps)
+} & (BodyProps | HeadingProps | DisplayProps)
 
 export const Text: FC<TextProps> = ({
   children,
   a11yLabel,
   bottomSpacing,
-  color,
+  tone = 'default',
   size = 'md',
   variant = 'body',
 }) => {
   const defaultColor = BaseColor()
+  const theme = useTheme()
+  let color
+
+  /**
+   * Convenience function to get typography token based on variant and size abbreviation
+   */
+  const getTypographyToken = (
+    variant:
+      | BodyProps['variant']
+      | HeadingProps['variant']
+      | DisplayProps['variant'],
+    size: TextSizes,
+  ) => {
+    const { typography } = font
+
+    const prefix = 'vadsFont'
+
+    const sizeMap = {
+      xs: 'Xsmall',
+      sm: 'Small',
+      md: 'Medium',
+      lg: 'Large',
+    }
+
+    let key
+
+    /** Build typography token key based on variant and size props */
+    switch (variant) {
+      case 'display':
+        key = `${prefix}Display`
+        break
+      case 'heading':
+        key = `${prefix}Heading${sizeMap[size as keyof typeof sizeMap]}`
+        break
+      default:
+        key = `${prefix}Body${sizeMap[size as keyof typeof sizeMap]}`
+    }
+
+    return typography[key as keyof typeof typography]
+  }
+
+  const getColor = (tone: BodyProps['tone']): string => {
+    switch (tone) {
+      case 'subtle':
+        return theme.vadsColorForegroundSubtle
+      case 'inverse':
+        return theme.vadsColorForegroundInverse
+      case 'error':
+        return theme.vadsColorForegroundError
+      default:
+        return theme.vadsColorForegroundDefault
+    }
+  }
 
   const style: TextStyle = {
     ...getTypographyToken(variant, size),
-    color: color ? colors[color] : defaultColor,
+    color: getColor(tone),
   }
 
   /** Set bottom margin to custom bottomSpacing if provided */
