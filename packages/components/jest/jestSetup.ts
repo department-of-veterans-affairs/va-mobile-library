da10043c6a6a14a2e54b93dd4333113c94a2a4d5
TimRoe <tim018@outlook.com>
2025-07-18 15:44:03 -0500

    Improved unit test fix

--- packages/components/jest/jestSetup.ts
+++ packages/components/jest/jestSetup.ts
@@ -1,6 +1,2 @@
 // Import built-in Jest matchers
 import '@testing-library/react-native/matchers'
-
-// Global React import for all test files since Jest requires it while React no longer does
-import React from 'react'
-global.React = React
